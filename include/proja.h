43524a7151c39fd121f5eaf20b34bc3a31784720
D Fouche <dylanfouche@gmail.com>
2019-09-26 20:14:58 +0100

    Initial code draft

---
+++ include/proja.h
@@ -0,0 +1,45 @@
+
+#ifndef _PROJ_A_H
+#define _PROJ_A_H
+
+//Includes
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <softPwm.h>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <pthread.h>
+#include <iostream>
+
+//Define buttons and leds
+#define ALARM_LED 1
+#define RESET_BTN 2
+#define ALARM_DISMISS_BTN 3
+#define INTERVAL_ADJUST_BTN 0
+#define START_STOP_BTN 7
+
+//SPI Settings
+#define SPI_CHAN 0
+#define SPI_SPEED 204800
+
+//Debounce time
+#define DEBOUNCE_TIME 200
+
+//Function definitions
+void setup_gpio(void);
+void setup_dac(void);
+void activate_alarm(void);
+void deactivate_alarm(void);
+void write_to_dac(char Vout);
+int main(void);
+
+//global variables for debouncing
+long last_interrupt_a = 0;
+long last_interrupt_b = 0;
+long last_interrupt_c = 0;
+long last_interrupt_d = 0;
+
+#endif
---
+++ src/proja.cpp
@@ -0,0 +1,104 @@
+
+#include "proja.h"
+
+using namespace std;
+
+/*
+ * main function
+ */
+int main()
+{
+    setup_gpio();
+    setup_dac();
+    //TODO:
+    //setup adc
+    //setup rtc
+    //setup blynk
+    while(true){
+      //TODO:
+      //read in from adc
+      //do some computation
+      //write out to dac
+      //publish data to blynk
+    }
+}
+
+/*
+ * Utility functions
+ */
+void write_to_dac(char Vout)
+{
+  char reg[2];
+  reg[0] = 0b01010000 | (Vout>>4);
+  reg[1] = Vout<<4;
+  wiringPiSPIDataRW(SPI_CHAN, (unsigned char*)reg, 2);
+}
+void activate_alarm(void)
+{
+  digitalWrite(ALARM_LED,1);
+}
+void deactivate_alarm(void)
+{
+  digitalWrite(ALARM_LED,0);
+}
+/*
+ * Interrupt handlers
+ */
+void reset_isr(void){
+    long interrupt_time = millis();
+    if(interrupt_time - last_interrupt_a > DEBOUNCE_TIME){
+        cout << "Reset button pressed" << endl;
+    }
+    last_interrupt_a = interrupt_time;
+}
+void alarm_dismiss_isr(void){
+    long interrupt_time = millis();
+    if(interrupt_time - last_interrupt_b > DEBOUNCE_TIME){
+        cout << "Alarm dismiss button pressed" << endl;
+    }
+    last_interrupt_b = interrupt_time;
+}
+void interval_adjust_isr(void){
+    long interrupt_time = millis();
+    if(interrupt_time - last_interrupt_c > DEBOUNCE_TIME){
+        cout << "Interval adjust button pressed" << endl;
+    }
+    last_interrupt_c = interrupt_time;
+}
+void start_stop_isr(void){
+    long interrupt_time = millis();
+    if(interrupt_time - last_interrupt_d > DEBOUNCE_TIME){
+        cout << "Start/stop button pressed" << endl;
+    }
+    last_interrupt_d = interrupt_time;
+}
+/*
+ * Setup functions
+ */
+void setup_gpio(void)
+{
+    //set up wiring Pi
+    wiringPiSetup();
+    //set up the buttons
+    pinMode(RESET_BTN, INPUT);
+    pinMode(ALARM_DISMISS_BTN, INPUT);
+    pinMode(INTERVAL_ADJUST_BTN, INPUT);
+    pinMode(START_STOP_BTN, INPUT);
+    pullUpDnControl(RESET_BTN, PUD_UP);
+    pullUpDnControl(ALARM_DISMISS_BTN, PUD_UP);
+    pullUpDnControl(INTERVAL_ADJUST_BTN, PUD_UP);
+    pullUpDnControl(START_STOP_BTN, PUD_UP);
+    //register interrupt handlers for buttons
+    wiringPiISR(RESET_BTN, INT_EDGE_RISING, &reset_isr);
+    wiringPiISR(ALARM_DISMISS_BTN, INT_EDGE_RISING, &alarm_dismiss_isr);
+    wiringPiISR(INTERVAL_ADJUST_BTN, INT_EDGE_RISING, &interval_adjust_isr);
+    wiringPiISR(START_STOP_BTN, INT_EDGE_RISING, &start_stop_isr);
+    //set up led
+    pinMode(ALARM_LED, OUTPUT);
+}
+
+void setup_dac(void)
+{
+  //set up the SPI interface
+  wiringPiSPISetup (SPI_CHAN, SPI_SPEED);
+}
